1f4582c364a01b60fc2f37d6f387858b4fc736bd
shrivarshan-07 <shri31007@gmail.com>
2026-05-22 09:39:01 +0530

    exp completed

--- simulation/qsim/work/@r@i@p@p@l@e@c@o@u@n@t@e@r/_primary.vhd
+++
@@ -1,9 +0,0 @@
-library verilog;
-use verilog.vl_types.all;
-entity RIPPLECOUNTER is
-    port(
-        q               : out    vl_logic_vector(3 downto 0);
-        clk             : in     vl_logic;
-        reset           : in     vl_logic
-    );
-end RIPPLECOUNTER;
--- simulation/qsim/work/@r@i@p@p@l@e@c@o@u@n@t@e@r_vlg_vec_tst/_primary.vhd
+++
@@ -1,4 +0,0 @@
-library verilog;
-use verilog.vl_types.all;
-entity RIPPLECOUNTER_vlg_vec_tst is
-end RIPPLECOUNTER_vlg_vec_tst;
